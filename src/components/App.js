ea1e921da2e5e02244aa9ddecdd106a23a333ebe
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-11-23 18:22:09 +0000

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hola mundo!</p>
+    </div>
+  );
+}
+
+export default App;
