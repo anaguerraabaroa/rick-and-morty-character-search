bbd8d58f61ed3506cb2e0ed780a412270a182189
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-11-24 17:18:30 +0000

    Update run docs

--- docs/static/js/main.582535f9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-template"]=this["webpackJsonpmy-template"]||[]).push([[0],{10:function(e,t,n){},11:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),s=n.n(r),i=n(3),o=n.n(i);n(9);var a=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("p",{children:"Hola mundo!"})})};n(10);o.a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.582535f9.chunk.js.map
