a0b22d54187237964b3d27435b8ed80bb451cbf6
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-11-24 17:17:29 +0000

    Add all components stylesheets with phone, tablet and desktop mediaqueries

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./components/App";
-import "./stylesheets/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
